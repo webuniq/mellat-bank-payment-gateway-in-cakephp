a157991849e000018c3bd5ab8958ff54a8fe0edd
webuniq <sahel_4b@yahoo.com>
2013-09-08 21:40:39 +0430

    Create core.php

---
+++ Config/core.php
@@ -0,0 +1,12 @@
+<?php
+.
+.
+.
+// - - - - - - - - Mellat Payment - - - - - - - - - -
+Configure::write('Settings.pay.terminalId', '00000');
+Configure::write('Settings.pay.userName', '--user--');
+Configure::write('Settings.pay.password', '--pass--');
+// - - - - - - - - Mellat Payment - - - - - - - - - -
+.
+.
+.
